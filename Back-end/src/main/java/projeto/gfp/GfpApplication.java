4c6d5b1d351e633175eb44b826e3a7623c5b4cdf
Marcelo <marcelodossantosmachado05@gmail.com>
2025-11-30 10:50:41 -0300

    Commit Inicial: Add arquivos iniciais

---
+++ Back-end/src/main/java/projeto/gfp/GfpApplication.java
@@ -0,0 +1,13 @@
+package projeto.gfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfpApplication.class, args);
+	}
+
+}
