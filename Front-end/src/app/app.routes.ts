0faf6cd6f3809fd17a797e85c167b796b68c398c
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-05 16:08:00 -0300

    Feat: Organizando routes

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './views/home/home';
+import { Gestoes } from './components/gestoes/gestoes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Home},
+    {path: "gestoes", component: Gestoes}
+];
