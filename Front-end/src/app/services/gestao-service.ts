ecd33e311fe41e716f024f1c36455c4667327bc6
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-05 16:04:40 -0300

    Feat: Add service de gestão

---
+++ Front-end/src/app/services/gestao-service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { GestaoModel } from '../shared/models/GestaoModel';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GestaoService {
+  
+  protected http = inject(HttpClient);
+  protected gestao! : GestaoModel;
+  protected urlApi: string = 'http://localhost:8080/api/gestao';
+  protected gestoes!: GestaoModel[];
+
+  public getGestao(): Observable<GestaoModel[]> {
+    return this.http.get<GestaoModel[]>(`${this.urlApi}/listar`);
+  }
+}
