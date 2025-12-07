ca8dbf8d044bf134b8a8e2f04064dc2cb12ee888
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-06 21:04:29 -0300

    Fix: Organizando rotas para criar acoes

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
-import { Home } from './views/home/home';
 import { Gestoes } from './components/gestoes/gestoes';
+import { CriarGestao } from './components/criar-gestao/criar-gestao';
+import { CriarAcao } from './components/criar-acao/criar-acao';
+import { Acao } from './components/acao/acao';
 
 export const routes: Routes = [
-    {path: "", component: Home},
-    {path: "gestoes", component: Gestoes}
+    {path: "", component: Gestoes},
+    {path: "criar/gestao", component: CriarGestao},
+    {path: "criar/acao", component: CriarAcao},
+    {path: "acao/gestao", component: Acao},
 ];
