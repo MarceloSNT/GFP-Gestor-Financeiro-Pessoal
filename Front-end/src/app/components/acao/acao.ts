a974977f83c269ffaff810d5d4dabaaeead4afbe
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-10 19:44:13 -0300

    Style: Mudando as ações de tabela para cards

--- Front-end/src/app/components/acao/acao.ts
+++ Front-end/src/app/components/acao/acao.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { NavBar } from "../nav-bar/nav-bar";
 import { AcaoModel } from '../../shared/models/AcaoModel';
 import { AçaoService } from '../../services/açao-service';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-acao',
-  imports: [RouterLink, NavBar],
+  imports: [RouterLink, NavBar, DatePipe, NgClass],
   templateUrl: './acao.html',
   styleUrl: './acao.scss',
 })
@@ -16,16 +17,20 @@ export class Acao implements OnInit{
   protected acoes : AcaoModel[] = [];
   protected acaoService = inject(AçaoService);
   protected route = inject(ActivatedRoute);
+  protected router = inject(Router);
   protected cdr = inject(ChangeDetectorRef);
 
-  ngOnInit() {
+  protected cdGestao!: number;
+
+ngOnInit() {
   this.route.queryParams.subscribe(params => {
-    const cdGestao = params['cdGestao'];
-    if (cdGestao) {
-      this.carregarAcoes(cdGestao);
+    this.cdGestao = Number(params['cdGestao']);
+    if (this.cdGestao) {
+      this.carregarAcoes(this.cdGestao);
     }
   });
 }
+
 
 carregarAcoes(cdGestao: number) {
   this.acaoService.getAcoesByCdGestao(cdGestao).subscribe(acoes => {
@@ -33,4 +38,11 @@ carregarAcoes(cdGestao: number) {
     this.cdr.detectChanges();
   });
 }
+
+criarAcaoNavegar() {
+  this.router.navigate(['/criar/acao'], {
+    queryParams: { cdGestao: this.cdGestao }
+  });
+}
+
 }
