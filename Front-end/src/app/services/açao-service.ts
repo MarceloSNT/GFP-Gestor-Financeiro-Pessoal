fc5fa9718e152345b1ed276881f9b462037c1642
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-13 01:43:23 -0300

    Feat: add Bearer token

--- Front-end/src/app/services/açao-service.ts
+++ Front-end/src/app/services/açao-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { GestaoModel } from '../shared/models/GestaoModel';
 import { AcaoModel } from '../shared/models/AcaoModel';
@@ -12,6 +12,8 @@ export class AçaoService {
   protected http = inject(HttpClient);
   protected acao! : AcaoModel;
   protected urlApi: string = 'http://localhost:8080/api/acao';
+  
+  
 
   public postAcao(payload: AcaoModel): Observable<AcaoModel> {
     return this.http.post<AcaoModel>(`${this.urlApi}/cadastro`, payload);
@@ -24,10 +26,17 @@ export class AçaoService {
   }
 
   public deleteAcao(cdAcao: number): Observable<void> {
-  return this.http.put<void>(
+  const token = localStorage.getItem('token');
+    
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.put<void>(
     `${this.urlApi}/desativar/ativar`,
     {},
     {
+      headers,
       params: { cdAcao: cdAcao }
     }
   );
--- Front-end/src/app/services/gestao-service.ts
+++ Front-end/src/app/services/gestao-service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { GestaoModel } from '../shared/models/GestaoModel';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,8 +13,11 @@ export class GestaoService {
   protected urlApi: string = 'http://localhost:8080/api/gestao';
   protected gestoes!: GestaoModel[];
 
-  public getGestao(): Observable<GestaoModel[]> {
-    return this.http.get<GestaoModel[]>(`${this.urlApi}/listar`);
+  public getGestao(cdUsuario : number): Observable<GestaoModel[]> {
+    return this.http.get<GestaoModel[]>(`${this.urlApi}/listar/usuario`,
+    {
+      params: { cdUsuario: cdUsuario }
+    });
   }
 
   public postGestao(payload: GestaoModel): Observable<GestaoModel> {
@@ -22,10 +25,17 @@ export class GestaoService {
   }
 
   public deletarGestao(cdGestao: number): Observable<void> {
+    const token = localStorage.getItem('token');
+    
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    
     return this.http.put<void>(
       `${this.urlApi}/desativar/ativar`,
       {},
       {
+        headers,
         params: {cdGestao: cdGestao}
       }
     );
