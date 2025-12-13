4ee5382151f5c6864f82d12675de62b05b76e608
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-13 01:50:36 -0300

    Feat: Add data.sql de status e tipo

---
+++ Back-end/src/main/resources/data.sql
@@ -0,0 +1,26 @@
+--STATUS
+INSERT INTO public.tbstatus
+(cdstatus, nmstatus)
+VALUES(1, 'RECEITA');
+
+INSERT INTO public.tbstatus
+(cdstatus, nmstatus)
+VALUES(2, 'DESPESA');
+--======================================================================================================================
+--TIPO
+INSERT INTO public.tbtipo
+(cdtipo, nmtipo)
+VALUES(1, 'PIX');
+
+INSERT INTO public.tbtipo
+(cdtipo, nmtipo)
+VALUES(2, 'DINHEIRO');
+
+INSERT INTO public.tbtipo
+(cdtipo, nmtipo)
+VALUES(3, 'CRÉDITO');
+
+INSERT INTO public.tbtipo
+(cdtipo, nmtipo)
+VALUES(4, 'DÉBITO');
+--======================================================================================================================
