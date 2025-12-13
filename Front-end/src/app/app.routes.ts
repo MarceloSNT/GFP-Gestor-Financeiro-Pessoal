d8d54f9d28b9ee562c01e8469fae0b313b05d2fb
Marcelo <marcelodossantosmachado05@gmail.com>
2025-12-13 01:48:06 -0300

    Feat: Add rotas para login e cadastro

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Gestoes } from './components/gestoes/gestoes';
 import { CriarGestao } from './components/criar-gestao/criar-gestao';
 import { CriarAcao } from './components/criar-acao/criar-acao';
 import { Acao } from './components/acao/acao';
+import { Login } from './components/login/login';
+import { Cadastro } from './components/cadastro/cadastro';
 
 export const routes: Routes = [
     {path: "", component: Gestoes},
     {path: "criar/gestao", component: CriarGestao},
     {path: "criar/acao", component: CriarAcao},
     {path: "acao/gestao", component: Acao},
+    {path: "login", component: Login},
+    {path: "cadastro", component: Cadastro}
 ];
